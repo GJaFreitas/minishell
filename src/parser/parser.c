a2ae63aa7f36cc433aea37a6d0de19e6012dd20f
GJaFreitas <gjacome-@student.42lisboa.com>
2025-06-23 17:37:42 +0100

    Remove quotes from cmd arguments

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,5 +1,31 @@
+#include "lexer.h"
+#include "libft.h"
 #include "minishell.h"
 #include "parser.h"
+
+static void	__rm_quote_arg(char *arg)
+{
+	while (*arg)
+	{
+		if (ft_strchr(QUOTES, *arg))
+			ft_memcpy(arg, arg + 1, ft_strlen(arg + 1) + 1);
+		else
+			arg++;
+	}
+}
+
+static void	__remove_quotes(t_cmd *cmds)
+{
+	int	i;
+
+	i = 0;
+	while (cmds)
+	{
+		while (cmds->args[i])
+			__rm_quote_arg(cmds->args[i++]);
+		cmds = cmds->next;
+	}
+}
 
 t_cmd	*parser(char **tokens)
 {
@@ -15,16 +41,8 @@ t_cmd	*parser(char **tokens)
 	#endif
 
 	cmds = assign_cmds(tokens);
+	__remove_quotes(cmds);
 	free_tokens(tokens);
 	free(tokens);
 	return (cmds);
-}
-
-void	free_tokens(char **tokens)
-{
-	int	i;
-
-	i = 0;
-	while (tokens[i])
-		free(tokens[i++]);
 }
--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -4,3 +4,12 @@ int	is_pipe(char *token)
 {
 	return (*token == '|');
 }
+
+void	free_tokens(char **tokens)
+{
+	int	i;
+
+	i = 0;
+	while (tokens[i])
+		free(tokens[i++]);
+}
