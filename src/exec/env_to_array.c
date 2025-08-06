208de7424a315bd2df5459be50cd53cf557da7d9
F5574 <gabrielvonah04@outlook.com>
2025-08-06 17:46:42 +0100

    to merge

--- src/exec/env_to_array.c
+++ src/exec/env_to_array.c
@@ -6,7 +6,7 @@
 /*   By: gvon-ah- <gvon-ah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/06 16:38:37 by gvon-ah-          #+#    #+#             */
-/*   Updated: 2025/08/06 16:46:44 by gvon-ah-         ###   ########.fr       */
+/*   Updated: 2025/08/06 17:44:18 by gvon-ah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,18 @@ static int count_envs(t_env *input)
 	return (i);
 }
 
+static int count_envs_pointers(char **input)
+{
+	int i;
+	char **current;
+
+	i = 0;
+	current = input;
+	while (current[i])
+		i++;
+	return (i);
+}
+
 char **env_to_array(t_env *env)
 {
     int count;
@@ -38,6 +50,36 @@ char **env_to_array(t_env *env)
 	count = count_envs(env);
     env_array = malloc(sizeof(char *) * (count + 1));
     if (!env_array)
+        return (NULL);
+    current = env;
+    while (current)
+    {
+        env_array[i] = ft_strjoin(ft_strjoin(current->key, "="), current->value);
+        if (!env_array[i])
+        {
+            while (i > 0)
+                free(env_array[--i]);
+            return (free(env_array),NULL);
+        }
+        current = current->next;
+        i++;
+    }
+    env_array[i] = NULL;
+    return (env_array);
+}
+
+
+t_env *array_to_env(char **env)
+{
+    int count;
+    char **env_c;
+    int i;
+	t_env env_t;
+
+	i = 0;
+	count = count_envs_pointers(env);
+    env_t = malloc(sizeof(t_env));
+    if (!env_t)
         return (NULL);
     current = env;
     while (current)
