c7555c6febc631ed73a25bcb57cbc42933088080
GJaFreitas <gjacome-@student.42lisboa.com>
2025-08-12 18:32:10 +0100

    Start of CD

--- src/functions/ft_cd.c
+++ src/functions/ft_cd.c
@@ -2,8 +2,13 @@
 
 int	ft_cd(char *const argv[], t_env *env)
 {
-	(void)argv;
+	char	*full_dir_path;
+
+	full_dir_path = get_full_dir_path(argv + 1);
+	if (path_exists(full_dir_path))
+		chdir(full_dir_path);
+	else
+		printf("Error");
 	(void)env;
-	printf("Implement cd\n");
 	exit(0);
 }
