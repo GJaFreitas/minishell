62ce078bd393b9032f4cb210aeba2f6e6b1dfc11
GJaFreitas <gjacome-@student.42lisboa.com>
2025-08-29 17:33:51 +0100

    Might do it with pipes

--- src/exec/heredoc.c
+++ src/exec/heredoc.c
@@ -3,6 +3,7 @@
 #include "parser.h"
 #include <fcntl.h>
 #include <readline/readline.h>
+#include <stdio.h>
 
 void	get_lines(int fd, const char *delimiter)
 {
@@ -20,35 +21,15 @@ void	get_lines(int fd, const char *delimiter)
 	free(line);
 }
 
-void	get_filename(char f[16])
-{
-	int	fd;
-	int	i;
-
-	fd = open("/dev/random", O_RDONLY);
-	read(fd, f, 15);
-	i = 0;
-	while (i < 15)
-	{
-		if (!ft_isprint(f[i]))
-			while (!ft_isprint(f[i]))
-				read(fd, &f[i], 1);
-		i++;
-	}
-	f[21] = 0;
-}
-
 // @TODO: Handle de sinais
 int	heredoc(char *delimiter, char **cmd_args)
 {
-	char			filename[32];
-	int				fd;
+	int				fd[2];
 
 	if (cmd_args[1] != 0)
 		return (-1);
-	ft_strlcpy(filename, "/tmp/", 6);
-	get_filename(&filename[5]);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
-	get_lines(fd, delimiter);
-	return (fd);
+	if (pipe(fd) == -1)
+		return (perror("Pipe:"), -1);
+	get_lines(fd[1], delimiter);
+	return (fd[0]);
 }
