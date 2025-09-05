6b7e939f6fe83e098e0e8100c6321c61fc1f5efc
GJaFreitas <gjacome-@student.42lisboa.com>
2025-09-05 16:43:15 +0100

    falta norm

--- src/parser/heredoc/heredoc.c
+++ src/parser/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: gvon-ah- <gvon-ah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 19:47:23 by bag               #+#    #+#             */
-/*   Updated: 2025/09/05 16:34:14 by bag              ###   ########.fr       */
+/*   Updated: 2025/09/05 16:39:21 by bag              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,11 @@ int	heredoc(char **delimiter, t_env *env)
 	pid_t	pid;
 	int		fd;
 
-	signal(SIGINT, SIG_IGN);
-	pid = fork();
-	if (pid == -1)
-		return (perror("Fork:"), -1);
+	(signal(SIGINT, SIG_IGN), pid = fork());
 	if (pid == 0)
 	{
 		signal(SIGINT, SIG_DFL);
 		fd = open("/tmp/hdoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		printf("delimiter: %s\n", delimiter[2]);
 		pid = get_lines(fd, delimiter[2], env_to_array(env));
 		if ((pid >> 31) & 1)
 		{
@@ -68,18 +64,11 @@ int	heredoc(char **delimiter, t_env *env)
 			close(fd);
 			fd = open("/tmp/hdoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		}
-		free_env(env);
-		free_tokens(delimiter);
-		free(delimiter);
-		close(fd);
+		(free_env(env), free_tokens(delimiter), free(delimiter), close(fd));
 		exit(g_sig);
 	}
 	else
-	{
-		waitpid(pid, &g_sig, 0);
-		write(1, "\n", 1);
-		signal(SIGINT, __sigint_h);
-	}
+		(waitpid(pid, &g_sig, 0), write(1, "\n", 1), signal(SIGINT, __sigint_h));
 	if (g_sig == 0)
 		return (open("/tmp/hdoc", O_RDONLY));
 	return (-1);
